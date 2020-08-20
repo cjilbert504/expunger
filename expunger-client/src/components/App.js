36fc446cab9a88e25e7c86eec11ab278c2a418b0
Collin Jilbert <cjilbert504@gmail.com>
2020-08-20 18:25:28 -0500

    Import Feed and Menu components

--- expunger-client/src/components/App.js
+++ expunger-client/src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Feed from './Feed';
+import Menu from './Menu';
 
 const App = () => {
     return (
-        <div className="ui container">App</div>
+        <div className="ui container">
+            <Menu />
+            <Feed />
+        </div>
     );
 };
 
