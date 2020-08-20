c093f62eaa6fa59602ce639c840734b08d3a8066
Collin Jilbert <cjilbert504@gmail.com>
2020-08-20 18:26:21 -0500

    Stub out Menu component

---
+++ expunger-client/src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <div class="ui left fixed blue inverted vertical menu">
+            <div class="item">
+                <h4>EXPUNGER</h4>
+            </div>
+            <a class="item">Feed</a>
+            <a class="item">Profile</a>
+            <a class="item">Sign-in</a>
+        </div>
+    )
+}
+
+export default Menu;
