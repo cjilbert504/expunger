a3e2033a68df68b858fdbd875186cf87b77ac057
Collin Jilbert <cjilbert504@gmail.com>
2020-08-20 18:26:03 -0500

    Stub out Feed component

---
+++ expunger-client/src/components/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Feed extends React.Component {
+    render() {
+        return (
+            <div className="ui very padded blue inverted raised segment" style={{ boxShadow: "5px 5px 10px #888888", marginBottom: "100px", marginLeft: "200px" }}>
+                Feed
+            </div>
+        )
+    }
+};
+
+export default Feed;
