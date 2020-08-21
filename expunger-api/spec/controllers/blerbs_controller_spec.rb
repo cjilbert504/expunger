250f3df70b13eac5d5be69d84a0fa3018bf1abaf
Collin Jilbert <cjilbert504@gmail.com>
2020-08-21 13:14:48 -0500

    Create before block to make get request to index action before all examples

--- expunger-api/spec/controllers/blerbs_controller_spec.rb
+++ expunger-api/spec/controllers/blerbs_controller_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe BlerbsController, :type => :controller do
     describe "GET index" do
-        it "has a 200 status code" do
+        before do
             get :index
+        end
 
+        it "has a 200 status code" do
             expect(response.status).to eq(200)
         end
 
         it "responds with content type of application/json" do
-            expect(response.content_type).to eq("application/json")
+            expect(response.content_type).to eq("application/json; charset=utf-8")
         end
     end
 end
