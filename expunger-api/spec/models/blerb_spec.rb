d87c2873980d58c130dde8dc67618d1dcb840732
Collin Jilbert <cjilbert504@gmail.com>
2020-08-21 10:21:06 -0500

    Add spec to test blerb is associated to a user

--- expunger-api/spec/models/blerb_spec.rb
+++ expunger-api/spec/models/blerb_spec.rb
@@ -12,4 +12,10 @@ RSpec.describe Blerb, type: :model do
   it "has content attribute that has data of type String assigned to it" do
     expect(@blerb.content.class).to eq(String)
   end
+
+  it "is associated to a user" do
+    @blerb.build_user(username: "murdok2", email: "murdok2@test.com", password: "12345")
+
+    expect(@blerb.user.username).to eq("murdok2")
+  end
 end
