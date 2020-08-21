266521c1988b29df5bbb089f7e18634cd8197638
Collin Jilbert <cjilbert504@gmail.com>
2020-08-21 10:50:29 -0500

    Create three users each with three blerbs associated to them

--- expunger-api/db/seeds.rb
+++ expunger-api/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do
+    User.create(username: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password )
+end
+
+3.times do
+    user = User.first.blerbs.build(content: Faker::Quote.robin)
+    user.save
+end
+
+3.times do
+    user = User.second.blerbs.build(content: Faker::Quote.singular_siegler)
+    user.save
+end
+
+3.times do
+    user = User.last.blerbs.build(content: Faker::Quote.famous_last_words)
+    user.save
+end
