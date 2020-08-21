857db340aaa0e0e7dac3206628379cecdff54b02
Collin Jilbert <cjilbert504@gmail.com>
2020-08-21 11:02:33 -0500

    Refactor seeds.rb

--- expunger-api/db/seeds.rb
+++ expunger-api/db/seeds.rb
@@ -1,18 +1,5 @@
 3.times do
-    User.create(username: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password )
-end
-
-3.times do
-    user = User.first.blerbs.build(content: Faker::Quote.robin)
-    user.save
-end
-
-3.times do
-    user = User.second.blerbs.build(content: Faker::Quote.singular_siegler)
-    user.save
-end
-
-3.times do
-    user = User.last.blerbs.build(content: Faker::Quote.famous_last_words)
+    user = User.new(username: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password )
+    user.blerbs.build(content: Faker::Quote.famous_last_words)
     user.save
 end
