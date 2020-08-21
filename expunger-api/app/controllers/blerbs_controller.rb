074a6b5abcedd3a6c1ca52613ea0d4fbe58466a2
Collin Jilbert <cjilbert504@gmail.com>
2020-08-21 13:12:42 -0500

    Refactor render response from index action

--- expunger-api/app/controllers/blerbs_controller.rb
+++ expunger-api/app/controllers/blerbs_controller.rb
@@ -1,6 +1,6 @@
 class BlerbsController < ApplicationController 
     def index
         blerbs = Blerb.all
-        render json: blerbs
+        render json: BlerbSerializer.new(blerbs).serialized_json
     end
 end
