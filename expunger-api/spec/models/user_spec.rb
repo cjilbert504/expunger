964f351580643e0fc8764e5b214e9aa6ad4d68d9
Collin Jilbert <cjilbert504@gmail.com>
2020-08-20 20:31:26 -0500

    Config RSpec and require devise in config/app.rb; first spec for user model

--- expunger-api/spec/models/user_spec.rb
+++ expunger-api/spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a username" do 
+    user = User.new(username: "murdok")
+
+    expect(user.username).to eq("murdok")
+  end
 end
