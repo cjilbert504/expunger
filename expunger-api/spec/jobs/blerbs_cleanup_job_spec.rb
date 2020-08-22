e460bb19c05fd7fa9434d5c68435c33a5c63925f
Collin Jilbert <cjilbert504@gmail.com>
2020-08-21 23:59:19 -0500

    Add spec to check for job being enqueued

--- expunger-api/spec/jobs/blerbs_cleanup_job_spec.rb
+++ expunger-api/spec/jobs/blerbs_cleanup_job_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe BlerbsCleanupJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#perform_later" do 
+    it "destroys a blerb record whose created_at attribute is greater than 10 seconds from the current time" do 
+      blerb = Blerb.create(content: "This is a test blerb!!")
+
+      ActiveJob::Base.queue_adapter = :test
+      BlerbsCleanupJob.perform_later(blerb.id)
+
+      expect(BlerbsCleanupJob).to have_been_enqueued
+    end
+  end
 end
