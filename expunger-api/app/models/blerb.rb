f55ee6a7e8d34d29108ea3bac3e726cca1280839
Collin Jilbert <cjilbert504@gmail.com>
2020-08-24 10:16:59 -0500

    Remove code comments; Add in other attr for devise user

--- expunger-api/app/models/blerb.rb
+++ expunger-api/app/models/blerb.rb
@@ -4,13 +4,4 @@ class Blerb < ApplicationRecord
   validates :content, length: { maximum: 120 }
 
   scope :not_outdated, -> { where("created_at >= ?", 20.seconds.ago) }
-
-  # private
-  #   def self.destroy_old_blerbs
-  #     self.where('created_at >= ?', 20.seconds.ago).destroy_all
-  #   end
-
-    # after_create do 
-    #   BlerbsCleanupJob.perfom_later
-    # end
 end
