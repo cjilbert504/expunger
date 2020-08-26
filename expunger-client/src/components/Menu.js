79d9774b824de19c3be8a3bf768b9b9b516e11fe
Collin Jilbert <cjilbert504@gmail.com>
2020-08-26 17:23:27 -0500

    Add feature to conditionally render certain menu nav links

--- expunger-client/src/components/Menu.js
+++ expunger-client/src/components/Menu.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
 
-const Menu = () => {
-    return (
-        <div className="ui left fixed violet inverted vertical menu">
-            <div className="item">
-                <h4>
-                    <Link to="/" className="item">EXPUNGER</Link>
-                </h4>
+class Menu extends React.Component {
+    renderConditionalMenuItems = () => {
+        console.log(Object.keys(this.props.users).length);
+        if (Object.keys(this.props.users).length > 0) {
+            return (
+                <div>
+                    <Link to="/feed" className="item">Feed</Link>
+                    {/* <Link to="/profile" className="item">Profile</Link> */}
+                    <Link to="/logout" className="item">Log Out</Link>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    {/* <Link to="/profile" className="item">Profile</Link> */}
+                    <Link to="/signup" className="item">Sign-Up</Link>
+                    <Link to="/login" className="item">Log In</Link>
+                    <Link to="/logout" className="item">Log Out</Link>
+                </div>
+            )
+        }
+
+    }
+
+    render() {
+        return (
+            <div className="ui left fixed violet inverted vertical menu">
+                <div className="item">
+                    <h4>
+                        <Link to="/" className="item">EXPUNGER</Link>
+                    </h4>
+                </div>
+                { this.renderConditionalMenuItems() }
             </div>
-            <Link to="/feed" className="item">Feed</Link>
-            <Link to="/profile" className="item">Profile</Link>
-            <Link to="/signup" className="item">Sign-Up</Link>
-            <Link to="/login" className="item">Log In</Link>
-            <Link to="/logout" className="item">Log Out</Link>
-        </div>
-    )
+        )
+    }
 }
 
-export default Menu;
+const mapStateToProps = ({ users }) => {
+    return { users }
+}
+
+export default connect(mapStateToProps)(Menu);
