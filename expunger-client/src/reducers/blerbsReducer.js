41b0de18e768c2c646c35dcd1aebb8186ba8e421
Collin Jilbert <cjilbert504@gmail.com>
2020-08-26 19:24:06 -0500

    Add case for adding a new blerb

--- expunger-client/src/reducers/blerbsReducer.js
+++ expunger-client/src/reducers/blerbsReducer.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
     switch (action.type) {
         case "GET_BLERBS":
             return action.payload;
+        case "ADD_BLERB":
+            return [ ...state, action.payload ]
         default:
             return state;
     };
