6009fa80c6a434369915459bd2d29c63a22f925d
Collin Jilbert <cjilbert504@gmail.com>
2020-08-26 19:21:01 -0500

    Add blerb create action

--- expunger-api/app/controllers/blerbs_controller.rb
+++ expunger-api/app/controllers/blerbs_controller.rb
@@ -4,4 +4,16 @@ class BlerbsController < ApplicationController
 
         render json: BlerbSerializer.new(blerbs).serialized_json
     end
+
+    def create
+        blerb = Blerb.new(blerb_params)
+        blerb.save
+        render json: BlerbSerializer.new(blerb).serialized_json
+    end
+
+    private
+
+    def blerb_params
+        params.require(:blerb).permit(:content, :user_id)
+    end
 end
