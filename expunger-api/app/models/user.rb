15e34061a542ad3b4a0f96888346b30134d3a488
Collin Jilbert <cjilbert504@gmail.com>
2020-08-29 19:06:11 -0500

    Add validation for presence of username

--- expunger-api/app/models/user.rb
+++ expunger-api/app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
 #   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :username, presence: true
 
   has_many :blerbs
 end
